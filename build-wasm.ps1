f49b5e6fc46711fa4d7c62746e1df9168e368ceb
OverflowCat <OverflowCat@gmail.com>
2026-03-27 03:38:14 +0800

    feat: add `wasm` & `wasm-gc` targets

---
+++ build-wasm.ps1
@@ -0,0 +1,9 @@
+moon build --target wasm-gc
+Copy-Item _build/wasm-gc/release/build/cmd/wasm/wasm.wasm cmd/wasm/wasm-gc.wasm
+
+moon build --target wasm
+Copy-Item _build/wasm/release/build/cmd/wasm/wasm.wasm cmd/wasm/wasm.wasm
+
+Write-Host "Open http://localhost:8960 in your browser"
+python -m http.server 8960 -d cmd/wasm
+exit 0  # python exits with 1 on Ctrl-C; suppress that
